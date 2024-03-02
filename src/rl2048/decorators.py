17c1910977c28e78bfb907850341a5ee07c1850e
Avi Revah <avraham.a.revah@gmail.com>
2024-03-02 02:28:25 +0000

    feat(decorators): implement key and out flags with type overloading

--- src/rl2048/decorators.py
+++ src/rl2048/decorators.py
@@ -11,53 +11,178 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# ruff: noqa: E731 N801
 
 from collections.abc import Callable
 from functools import wraps
 from typing import (
     Concatenate,
+    Literal,
     ParamSpec,
     Protocol,
     TypeAlias,
     TypeVar,
     TypeVarTuple,
+    overload,
 )
 
 import equinox as eqx
+import jax
+from chex import PRNGKey
 from jaxtyping import PyTree
 
 T = TypeVar("T")
 In = ParamSpec("In")
 Out = TypeVarTuple("Out")
 
-WhereFn: TypeAlias = Callable[[T], PyTree]
-PureFn: TypeAlias = Callable[Concatenate[T, In], tuple[PyTree, *Out]]
-ImpureFn: TypeAlias = Callable[Concatenate[T, In], tuple[T, *Out]]
+
+class T_K_Protocol(Protocol):
+    key: PRNGKey
 
 
-class MutableDecorator(Protocol):
-    def __call__(self, pure_fn: PureFn[T, In, *Out]) -> ImpureFn[T, In, *Out]:
+T_K = TypeVar("T_K", bound=T_K_Protocol)
+
+
+PureFn_: TypeAlias = Callable[Concatenate[T, In], PyTree]
+PureFn_K: TypeAlias = Callable[Concatenate[T_K, PRNGKey, In], PyTree | None]
+PureFn_O: TypeAlias = Callable[Concatenate[T, In], tuple[PyTree, *Out]]
+PureFn_KO: TypeAlias = Callable[
+    Concatenate[T_K, PRNGKey, In], tuple[PyTree | None, *Out]
+]
+ImpureFn: TypeAlias = Callable[Concatenate[T, In], T]
+ImpureFn_K: TypeAlias = ImpureFn[T_K, In]
+ImpureFn_O: TypeAlias = Callable[Concatenate[T, In], tuple[T, *Out]]
+ImpureFn_KO: TypeAlias = ImpureFn_O[T_K, In, *Out]
+
+
+class MutableDecorator_(Protocol):
+    def __call__(self, pure_fn: PureFn_[T, In]) -> ImpureFn[T, In]:
         ...
 
 
-def mutates(where: str | WhereFn[T]) -> MutableDecorator:
-    def decorator(pure_fn: PureFn[T, In, *Out]) -> ImpureFn:
-        if isinstance(where, str):
-            attr_names = where.split(",")
-            if len(attr_names) == 1:
-                where_fn = lambda pytree: getattr(pytree, attr_names[0])  # noqa: E731
-            else:
-                where_fn = lambda pytree: tuple(getattr(pytree, n) for n in attr_names)  # noqa: E731
-        else:
-            where_fn = where
+class MutableDecorator_K(Protocol):
+    def __call__(self, pure_fn: PureFn_K[T_K, In]) -> ImpureFn_K[T_K, In]:
+        ...
 
+
+class MutableDecorator_O(Protocol):
+    def __call__(self, pure_fn: PureFn_O[T, In, *Out]) -> ImpureFn_O[T, In, *Out]:
+        ...
+
+
+class MutableDecorator_KO(Protocol):
+    def __call__(self, pure_fn: PureFn_KO[T_K, In, *Out]) -> ImpureFn_KO[T_K, In, *Out]:
+        ...
+
+
+MutableDecorator: TypeAlias = (
+    MutableDecorator_ | MutableDecorator_K | MutableDecorator_O | MutableDecorator_KO
+)
+
+
+@overload
+def mutates(
+    where: str, *, key: Literal[False] = False, out: Literal[False] = False
+) -> MutableDecorator_:
+    ...
+
+
+@overload
+def mutates(
+    where: str | None = None, *, key: Literal[True], out: Literal[False] = False
+) -> MutableDecorator_K:
+    ...
+
+
+@overload
+def mutates(
+    where: str, *, key: Literal[False] = False, out: Literal[True]
+) -> MutableDecorator_O:
+    ...
+
+
+@overload
+def mutates(
+    where: str | None = None, *, key: Literal[True], out: Literal[True]
+) -> MutableDecorator_KO:
+    ...
+
+
+def mutates(
+    where: str | None = None,
+    *,
+    key: Literal[True, False] = False,
+    out: Literal[True, False] = False,
+) -> MutableDecorator:
+    attr_names = _make_attr_names(where, key=key)
+    where_fn = lambda pytree: [getattr(pytree, name) for name in attr_names]
+
+    def decorator_(pure_fn: PureFn_[T, In]) -> ImpureFn[T, In]:
         @wraps(pure_fn)
-        def impure_fn(pytree: T, *args: In.args, **kwargs: In.kwargs) -> tuple[T, *Out]:
-            updated_state, *output = pure_fn(pytree, *args, **kwargs)
-            updated_pytree: T = eqx.tree_at(where_fn, pytree, updated_state)
-            return (updated_pytree, *output)  # type: ignore[return-value]
+        def impure_fn(pytree: T, *args: In.args, **kwargs: In.kwargs) -> T:
+            updates = pure_fn(pytree, *args, **kwargs)
+            updates_flat = _flatten_updates(updates, attr_names)
+            return eqx.tree_at(where_fn, pytree, updates_flat)  # type: ignore[no-any-return]
 
         return impure_fn
 
-    return decorator
+    def decorator_k(pure_fn: PureFn_K[T_K, In]) -> ImpureFn[T_K, In]:
+        # @wraps(pure_fn)
+        def impure_fn(pytree: T_K, *args: In.args, **kwargs: In.kwargs) -> T_K:
+            next_key, sub_key = jax.random.split(pytree.key)
+            updates = pure_fn(pytree, sub_key, *args, **kwargs)
+            updates = {} if updates is None else updates
+            updates["key"] = next_key
+            updates_flat = _flatten_updates(updates, attr_names)
+            return eqx.tree_at(where_fn, pytree, updates_flat)  # type: ignore[no-any-return]
+
+        return impure_fn
+
+    def decorator_o(pure_fn: PureFn_O[T, In, *Out]) -> ImpureFn_O[T, In, *Out]:
+        @wraps(pure_fn)
+        def impure_fn(pytree: T, *args: In.args, **kwargs: In.kwargs) -> tuple[T, *Out]:
+            updates, *outputs = pure_fn(pytree, *args, **kwargs)
+            updates_flat = _flatten_updates(updates, attr_names)
+            return (eqx.tree_at(where_fn, pytree, updates_flat), *outputs)  # type: ignore[return-value]
+
+        return impure_fn
+
+    def decorator_ko(pure_fn: PureFn_KO[T_K, In, *Out]) -> ImpureFn_O[T_K, In, *Out]:
+        # @wraps(pure_fn)
+        def impure_fn(
+            pytree: T_K, *args: In.args, **kwargs: In.kwargs
+        ) -> tuple[T_K, *Out]:
+            next_key, sub_key = jax.random.split(pytree.key)
+            updates, *outputs = pure_fn(pytree, sub_key, *args, **kwargs)
+            updates = {} if updates is None else updates
+            updates["key"] = next_key
+            updates_flat = _flatten_updates(updates, attr_names)
+            return (eqx.tree_at(where_fn, pytree, updates_flat), *outputs)  # type: ignore[return-value]
+
+        return impure_fn
+
+    match key, out:
+        case False, False:
+            return decorator_
+        case True, False:
+            return decorator_k
+        case False, True:
+            return decorator_o
+        case True, True:
+            return decorator_ko
+
+    msg = f"Cannot build mutates decorator with parameters {where=}, {key=}, {out=}."
+    raise ValueError(msg)
+
+
+def _make_attr_names(where: str | None, *, key: bool) -> list[str]:
+    if where is None and not key:
+        msg = f"{mutates.__name__} requires 'key' to be 'True' when 'where = None'"
+        raise ValueError(msg)
+
+    attr_names = [] if where is None else where.split(",")
+    return ["key", *attr_names] if key else attr_names
+
+
+def _flatten_updates(updates: dict[str, PyTree], attr_names: list[str]) -> list[PyTree]:
+    return [updates[name] for name in attr_names]
--- tests/decorators_test.py
+++ tests/decorators_test.py
@@ -43,22 +43,20 @@ class CoinCounter(eqx.Module):
 
     @eqx.filter_jit
     @mutates("n_coins,total")
-    def count(self, coin: Array) -> tuple[tuple[Array, Array]]:
-        return ((self.n_coins + 1, self.total + coin),)
+    def count(self, coin: Array) -> dict[str, Array]:
+        return {"n_coins": self.n_coins + 1, "total": self.total + coin}
 
     @eqx.filter_jit
     @mutates("object")
-    def new_object(self) -> tuple[tuple[Any]]:
+    def new_object(self) -> dict[str, Any]:
         del self
-        return ((object(),),)
+        return {"object": object()}
 
     @eqx.filter_jit
-    @mutates("key,n_coins,total")
-    def count_random(self) -> tuple[tuple[Array, Array, Array], Array]:
-        next_key, rand_key = jax.random.split(self.key)
-        rand = jax.random.randint(rand_key, (), 1, 10**6)
-        del rand_key
-        return (next_key, self.n_coins + 1, self.total + rand), rand
+    @mutates("n_coins,total", key=True, out=True)
+    def count_random(self, key: PRNGKey) -> tuple[dict[str, Array], Array]:
+        rand = jax.random.randint(key, (), 1, 10**6)
+        return {"n_coins": self.n_coins + 1, "total": self.total + rand}, rand
 
 
 @pytest.fixture()
@@ -69,14 +67,14 @@ def counter() -> CoinCounter:
 def test_count(counter: CoinCounter, jit: bool) -> None:
     with chex.fake_jit(not jit):
         for coin in range(1, 10):
-            (counter,) = counter.count(jnp.asarray(coin))
+            counter = counter.count(jnp.asarray(coin))
             assert counter.n_coins == coin
             assert counter.total == (coin * (coin + 1)) // 2
 
         count = eqx.filter_jit(chex.assert_max_traces(counter.__class__.count, 1))
         chex.clear_trace_counter()
         for coin in range(10, 20):
-            (counter,) = count(counter, jnp.asarray(coin))
+            counter = count(counter, jnp.asarray(coin))
             assert counter.n_coins == coin
             assert counter.total == (coin * (coin + 1)) // 2
 
@@ -84,13 +82,13 @@ def test_count(counter: CoinCounter, jit: bool) -> None:
 def test_new_object(counter: CoinCounter, jit: bool) -> None:
     with chex.fake_jit(not jit):
         prev_object = counter.object
-        (counter,) = counter.new_object()
+        counter = counter.new_object()
         assert counter.object != prev_object
 
         new_object = counter.__class__.new_object
         new_object = eqx.filter_jit(chex.assert_max_traces(new_object, 1))
         chex.clear_trace_counter()
-        (counter,) = new_object(counter)
+        counter = new_object(counter)
 
         if jit:
             with pytest.raises(AssertionError):
@@ -112,3 +110,6 @@ def test_count_random(counter: CoinCounter, jit: bool) -> None:
             counter, random = count_random(counter)
             assert random != last_random
             last_random = random
+
+
+CoinCounter().count_random()
