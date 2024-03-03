0310977e9b2f4006c0f710fa937d17f0022139d0
Avi Revah <avraham.a.revah@gmail.com>
2024-03-03 01:11:51 +0000

    feat(decorators): better typing

--- src/rl2048/decorators.py
+++ src/rl2048/decorators.py
@@ -11,11 +11,11 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-# ruff: noqa: E731 N801
 
 from collections.abc import Callable
 from functools import wraps
 from typing import (
+    Any,
     Concatenate,
     Literal,
     ParamSpec,
@@ -30,81 +30,75 @@ import equinox as eqx
 import jax
 from chex import ArrayTree, PRNGKey
 
-T = TypeVar("T")
-In = ParamSpec("In")
-Out = TypeVarTuple("Out")
 
-
-class T_K_Protocol(Protocol):
+class TwKProtocol(Protocol):
     key: PRNGKey
 
 
-T_K = TypeVar("T_K", bound=T_K_Protocol)
+T = TypeVar("T")
+P = ParamSpec("P")
+Ts = TypeVarTuple("Ts")
+TwK = TypeVar("TwK", bound=TwKProtocol)
+
+Dict: TypeAlias = dict[str, ArrayTree]
+_PureFn: TypeAlias = Callable[Concatenate[T, P], Dict]
+_PureFnwK: TypeAlias = Callable[Concatenate[TwK, PRNGKey, P], Dict | None]
+_PureFnwO: TypeAlias = Callable[Concatenate[T, P], tuple[Dict, *Ts]]
+_PureFnwKwO: TypeAlias = Callable[Concatenate[TwK, PRNGKey, P], tuple[Dict | None, *Ts]]
+_ImpureFn: TypeAlias = Callable[Concatenate[T, P], T]
+_ImpureFnwO: TypeAlias = Callable[Concatenate[T, P], tuple[T, *Ts]]
 
 
-Update: TypeAlias = dict[str, ArrayTree]
-PureFn_: TypeAlias = Callable[Concatenate[T, In], Update]
-PureFn_K: TypeAlias = Callable[Concatenate[T_K, PRNGKey, In], Update | None]
-PureFn_O: TypeAlias = Callable[Concatenate[T, In], tuple[Update, *Out]]
-PureFn_KO: TypeAlias = Callable[
-    Concatenate[T_K, PRNGKey, In], tuple[Update | None, *Out]
-]
-ImpureFn: TypeAlias = Callable[Concatenate[T, In], T]
-ImpureFn_K: TypeAlias = ImpureFn[T_K, In]
-ImpureFn_O: TypeAlias = Callable[Concatenate[T, In], tuple[T, *Out]]
-ImpureFn_KO: TypeAlias = ImpureFn_O[T_K, In, *Out]
-
-
-class MutableDecorator_(Protocol):
-    def __call__(self, pure_fn: PureFn_[T, In]) -> ImpureFn[T, In]:
+class _MutatesWrapper(Protocol):
+    def __call__(self, pure_fn: _PureFn[T, P]) -> _ImpureFn[T, P]:
         ...
 
 
-class MutableDecorator_K(Protocol):
-    def __call__(self, pure_fn: PureFn_K[T_K, In]) -> ImpureFn_K[T_K, In]:
+class _MutatesWrapperwK(Protocol):
+    def __call__(self, pure_fn: _PureFnwK[TwK, P]) -> _ImpureFn[TwK, P]:
         ...
 
 
-class MutableDecorator_O(Protocol):
-    def __call__(self, pure_fn: PureFn_O[T, In, *Out]) -> ImpureFn_O[T, In, *Out]:
+class _MutatesWrapperwO(Protocol):
+    def __call__(self, pure_fn: _PureFnwO[T, P, *Ts]) -> _ImpureFnwO[T, P, *Ts]:
         ...
 
 
-class MutableDecorator_KO(Protocol):
-    def __call__(self, pure_fn: PureFn_KO[T_K, In, *Out]) -> ImpureFn_KO[T_K, In, *Out]:
+class _MutatesWrapperwKwO(Protocol):
+    def __call__(self, pure_fn: _PureFnwKwO[TwK, P, *Ts]) -> _ImpureFnwO[TwK, P, *Ts]:
         ...
 
 
-MutableDecorator: TypeAlias = (
-    MutableDecorator_ | MutableDecorator_K | MutableDecorator_O | MutableDecorator_KO
+MutatesWrapper: TypeAlias = (
+    _MutatesWrapper | _MutatesWrapperwK | _MutatesWrapperwO | _MutatesWrapperwKwO
 )
 
 
 @overload
 def mutates(
     where: str, *, key: Literal[False] = False, out: Literal[False] = False
-) -> MutableDecorator_:
+) -> _MutatesWrapper:
     ...
 
 
 @overload
 def mutates(
     where: str | None = None, *, key: Literal[True], out: Literal[False] = False
-) -> MutableDecorator_K:
+) -> _MutatesWrapperwK:
     ...
 
 
 @overload
 def mutates(
     where: str, *, key: Literal[False] = False, out: Literal[True]
-) -> MutableDecorator_O:
+) -> _MutatesWrapperwO:
     ...
 
 
 @overload
 def mutates(
     where: str | None = None, *, key: Literal[True], out: Literal[True]
-) -> MutableDecorator_KO:
+) -> _MutatesWrapperwKwO:
     ...
 
 
@@ -113,52 +107,63 @@ def mutates(
     *,
     key: Literal[True, False] = False,
     out: Literal[True, False] = False,
-) -> MutableDecorator:
+    ensure_jit: bool = True,
+) -> MutatesWrapper:
     attr_names = _make_attr_names(where, key=key)
-    where_fn = lambda pytree: [getattr(pytree, name) for name in attr_names]
 
-    def decorator_(pure_fn: PureFn_[T, In]) -> ImpureFn[T, In]:
+    def where_fn(pytree: Any) -> ArrayTree:
+        return [getattr(pytree, name) for name in attr_names]
+
+    def decorator_(pure_fn: _PureFn[T, P]) -> _ImpureFn[T, P]:
         @wraps(pure_fn)
-        def impure_fn(pytree: T, *args: In.args, **kwargs: In.kwargs) -> T:
+        def impure_fn(pytree: T, *args: P.args, **kwargs: P.kwargs) -> T:
             updates = pure_fn(pytree, *args, **kwargs)
+            _ensure_jit(where_fn, pytree, updates, ensure_jit=ensure_jit)
             updates_flat = _flatten_updates(updates, attr_names)
-            return eqx.tree_at(where_fn, pytree, updates_flat)  # type: ignore[no-any-return]
+            new_pytree: T = eqx.tree_at(where_fn, pytree, updates_flat)
+            return new_pytree
 
         return impure_fn
 
-    def decorator_k(pure_fn: PureFn_K[T_K, In]) -> ImpureFn[T_K, In]:
+    def decorator_k(pure_fn: _PureFnwK[TwK, P]) -> _ImpureFn[TwK, P]:
         @wraps(pure_fn)
-        def impure_fn(pytree: T_K, *args: In.args, **kwargs: In.kwargs) -> T_K:
+        def impure_fn(pytree: TwK, *args: P.args, **kwargs: P.kwargs) -> TwK:
             next_key, sub_key = jax.random.split(pytree.key)
             updates = pure_fn(pytree, sub_key, *args, **kwargs)
             updates = {} if updates is None else updates
             updates["key"] = next_key
+            _ensure_jit(where_fn, pytree, updates, ensure_jit=ensure_jit)
             updates_flat = _flatten_updates(updates, attr_names)
-            return eqx.tree_at(where_fn, pytree, updates_flat)  # type: ignore[no-any-return]
+            new_pytree: TwK = eqx.tree_at(where_fn, pytree, updates_flat)
+            return new_pytree
 
         del impure_fn.__wrapped__  # type: ignore[attr-defined]
         return impure_fn
 
-    def decorator_o(pure_fn: PureFn_O[T, In, *Out]) -> ImpureFn_O[T, In, *Out]:
+    def decorator_o(pure_fn: _PureFnwO[T, P, *Ts]) -> _ImpureFnwO[T, P, *Ts]:
         @wraps(pure_fn)
-        def impure_fn(pytree: T, *args: In.args, **kwargs: In.kwargs) -> tuple[T, *Out]:
+        def impure_fn(pytree: T, *args: P.args, **kwargs: P.kwargs) -> tuple[T, *Ts]:
             updates, *outputs = pure_fn(pytree, *args, **kwargs)
+            _ensure_jit(where_fn, pytree, updates, ensure_jit=ensure_jit)
             updates_flat = _flatten_updates(updates, attr_names)
-            return (eqx.tree_at(where_fn, pytree, updates_flat), *outputs)  # type: ignore[return-value]
+            new_pytree: T = eqx.tree_at(where_fn, pytree, updates_flat)
+            return (new_pytree, *outputs)  # type: ignore[return-value]
 
         return impure_fn
 
-    def decorator_ko(pure_fn: PureFn_KO[T_K, In, *Out]) -> ImpureFn_O[T_K, In, *Out]:
+    def decorator_ko(pure_fn: _PureFnwKwO[TwK, P, *Ts]) -> _ImpureFnwO[TwK, P, *Ts]:
         @wraps(pure_fn)
         def impure_fn(
-            pytree: T_K, *args: In.args, **kwargs: In.kwargs
-        ) -> tuple[T_K, *Out]:
+            pytree: TwK, *args: P.args, **kwargs: P.kwargs
+        ) -> tuple[TwK, *Ts]:
             next_key, sub_key = jax.random.split(pytree.key)
             updates, *outputs = pure_fn(pytree, sub_key, *args, **kwargs)
             updates = {} if updates is None else updates
             updates["key"] = next_key
+            _ensure_jit(where_fn, pytree, updates, ensure_jit=ensure_jit)
             updates_flat = _flatten_updates(updates, attr_names)
-            return (eqx.tree_at(where_fn, pytree, updates_flat), *outputs)  # type: ignore[return-value]
+            new_pytree: TwK = eqx.tree_at(where_fn, pytree, updates_flat)
+            return (new_pytree, *outputs)  # type: ignore[return-value]
 
         del impure_fn.__wrapped__  # type: ignore[attr-defined]
         return impure_fn
@@ -179,14 +184,32 @@ def mutates(
 
 def _make_attr_names(where: str | None, *, key: bool) -> list[str]:
     if where is None and not key:
-        msg = f"{mutates.__name__} requires 'key' to be 'True' when 'where = None'"
+        msg = f"{mutates.__name__} requires 'key' to be 'True' when 'where' is 'None'"
         raise ValueError(msg)
 
     attr_names = [] if where is None else where.split(",")
     return ["key", *attr_names] if key else attr_names
 
 
+def _ensure_jit(
+    where_fn: Callable[[Any], ArrayTree],
+    pytree: Any,
+    updates: dict[str, ArrayTree],
+    *,
+    ensure_jit: bool,
+) -> None:
+    if ensure_jit:
+        _assert_is_array_tree(where_fn(pytree))
+        _assert_is_array_tree(updates)
+
+
 def _flatten_updates(
     updates: dict[str, ArrayTree], attr_names: list[str]
 ) -> list[ArrayTree]:
     return [updates[name] for name in attr_names]
+
+
+def _assert_is_array_tree(tree: Any) -> None:
+    if not all(eqx.is_array(leaf) for leaf in jax.tree_flatten(tree)[0]):
+        msg = "Attributes changed by `mutates` must be ArrayTrees in order to use jit."
+        raise TypeError(msg)
